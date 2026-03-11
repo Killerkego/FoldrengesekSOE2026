e27b66fd07d28ba98916f00df4541a7f85936498
gludovatza <attila@gludovatz.hu>
2026-03-11 15:00:17 +0100

    Adatszolgáltatás létrehozása és használata

--- FoldrengesekSOE2026/Program.cs
+++ FoldrengesekSOE2026/Program.cs
@@ -1,3 +1,6 @@
+using FoldrengesekSOE2026.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace FoldrengesekSOE2026
 {
     public class Program
@@ -8,6 +11,9 @@ namespace FoldrengesekSOE2026
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<FoldrengesContext>(options =>
+                options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection")!));
 
             var app = builder.Build();
 
