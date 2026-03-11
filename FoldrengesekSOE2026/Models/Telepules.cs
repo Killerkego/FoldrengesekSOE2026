9ec59de89daf5fc3e6bc83f1db9c8709fe60d800
gludovatza <attila@gludovatz.hu>
2026-03-11 14:46:14 +0100

    Modell osztályok

---
+++ FoldrengesekSOE2026/Models/Telepules.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoldrengesekSOE2026.Models
+{
+    public class Telepules
+    {
+        public int ID { get; set; } // a VS kitalálja, hogy ez egy elsődleges kulcs lesz!
+        [Required]
+        [Display(Name = "Település név")]
+        public string Nev { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Vármegye")]
+        public string Varmegye { get; set; } = string.Empty;
+
+        // Navigáció: településhez tartozó naplóbejegyzések lekéréséhez:
+        public virtual ICollection<Naplo>? Naplok { get; set; } = new List<Naplo>();
+
+    }
+}
