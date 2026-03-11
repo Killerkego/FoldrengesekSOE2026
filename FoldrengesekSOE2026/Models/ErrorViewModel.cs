3364e4cfd818a8883e1edba951bf515a558f44d5
gludovatza <attila@gludovatz.hu>
2026-03-11 14:04:58 +0100

    Initial FoldrengesekSOE2026 - ASP.NET Core MVC project

---
+++ FoldrengesekSOE2026/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoldrengesekSOE2026.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
