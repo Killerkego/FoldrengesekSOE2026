2474922af13769cf99a03b17350ba2835f286c1a
Killerkego <miklos.botos@gmail.com>
2026-04-15 14:31:46 +0200

    Tesztelés megalapozása

---
+++ FoldrengesekSOE2026.Tests/TestDbFactory.cs
@@ -0,0 +1,16 @@
+﻿using FoldrengesekSOE2026.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoldrengesekSOE2026.Tests;
+
+public static class TestDbFactory
+{
+    public static FoldrengesContext CreateContext(string dbName)
+    {
+        var options = new DbContextOptionsBuilder<FoldrengesContext>()
+            .UseInMemoryDatabase(dbName)
+            .Options;
+
+        return new FoldrengesContext(options);
+    }
+}
